43740020522089c62784fd73924663ace5408bb3
VladTheImplier <tt_cox@hotmail.com>
2020-04-01 16:55:46 +0200

    Add files via upload

---
+++ precache-manifest.b4ab6928a24637239d5de58812a88482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1432910c96a7d8a9195a11add6350df2",
+    "url": "/exyucovid19/index.html"
+  },
+  {
+    "revision": "e5a5b4e335bc9eeb6237",
+    "url": "/exyucovid19/static/css/main.852300a2.chunk.css"
+  },
+  {
+    "revision": "dab41191a6bb2ac5e2bc",
+    "url": "/exyucovid19/static/js/2.3404d7a7.chunk.js"
+  },
+  {
+    "revision": "04f7cd4950736aa8433a06c72651bc8d",
+    "url": "/exyucovid19/static/js/2.3404d7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5a5b4e335bc9eeb6237",
+    "url": "/exyucovid19/static/js/main.813bc361.chunk.js"
+  },
+  {
+    "revision": "922985d3b42d312ad137",
+    "url": "/exyucovid19/static/js/runtime-main.5bd5013e.js"
+  },
+  {
+    "revision": "e0f49827dbbfd43ae1e045a0e3a872e7",
+    "url": "/exyucovid19/static/media/juga.e0f49827.png"
+  }
+]);
